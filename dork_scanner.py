c1c7cb89502aceb21693bc809e34a0e4f6dca194
simba0x41 <simba0x41@gmail.com>
2021-01-06 20:22:02 -0500

    changing name file

--- dork_scanner.py
+++ dork_scanner.py
@@ -1,36 +1,29 @@
-import requests,re
-from urllib.parse import unquote
-from bs4 import BeautifulSoup
-from docopt import docopt
-from time import time as timer
-from functools import partial
-from multiprocessing import Pool
+"""Tesla Google Scrapper
 
-def get_urls(search_string, start):
-    temp = []
-    url='https://www.google.com/search'
-    payload = {'q' : search_string,'start' : start}
-    my_headers = {'User-Agent' : 'Mozilla/11.0'}
-    r = requests.get(url,params=payload,headers=my_headers)
+Usage:
+  tesla.py <search> <pages> <processes>
+  tesla.py (-h | --help)
 
-    soup = BeautifulSoup(r.text,'html.parser')
-    divtags = soup.find_all('div',class_='kCrYT')
+Arguments:
+  <search>        String to be Searched
+  <pages>         Number of pages
+  <processes>     Number of parallel processes
 
-    for div in divtags:
-        try:
-            temp.append(unquote(re.search('url\?q=(.+?)\&sa', div.a['href']).group(1)))
-        except:
-            continue
-    return temp 
+Options:
+  -h, --help     Show this screen.
 
-def dorks(search, pages, processes):
-    result = []
-    make_request = partial( get_urls, search )
-    pagelist     = [ str(x*10) for x in range( 0, int(pages) ) ]
-    with Pool(processes) as p:
-        tmp = p.map(make_request, pagelist)
-    for x in tmp:
-        result.extend(x)
-    result=list(set(result))
-    return result
-
+"""
+from tesla import *
+ 
+if __name__ == '__main__':
+    arguments = docopt( __doc__, version='T3sla Google Scrapper' )
+    search    = arguments['<search>']
+    pages     = arguments['<pages>']
+    processes   = int(arguments['<processes>'])
+    start = timer()
+    result = dorks(search,pages, processes)
+    print ( *result, sep = '\n' )
+    print( '\nTotal URLs Scraped : %s ' % str( len( result ) ) )
+    print( 'Script Execution Time : %s ' % ( timer() - start, ) )
+ 
+ 
--- tesla.py
+++ tesla.py
@@ -1,29 +1,36 @@
-"""Tesla Google Scrapper
+import requests,re
+from urllib.parse import unquote
+from bs4 import BeautifulSoup
+from docopt import docopt
+from time import time as timer
+from functools import partial
+from multiprocessing import Pool
 
-Usage:
-  tesla.py <search> <pages> <processes>
-  tesla.py (-h | --help)
+def get_urls(search_string, start):
+    temp = []
+    url='https://www.google.com/search'
+    payload = {'q' : search_string,'start' : start}
+    my_headers = {'User-Agent' : 'Mozilla/11.0'}
+    r = requests.get(url,params=payload,headers=my_headers)
 
-Arguments:
-  <search>        String to be Searched
-  <pages>         Number of pages
-  <processes>     Number of parallel processes
+    soup = BeautifulSoup(r.text,'html.parser')
+    divtags = soup.find_all('div',class_='kCrYT')
 
-Options:
-  -h, --help     Show this screen.
+    for div in divtags:
+        try:
+            temp.append(unquote(re.search('url\?q=(.+?)\&sa', div.a['href']).group(1)))
+        except:
+            continue
+    return temp 
 
-"""
-from dork_scanner import *
- 
-if __name__ == '__main__':
-    arguments = docopt( __doc__, version='T3sla Google Scrapper' )
-    search    = arguments['<search>']
-    pages     = arguments['<pages>']
-    processes   = int(arguments['<processes>'])
-    start = timer()
-    result = dorks(search,pages, processes)
-    print ( *result, sep = '\n' )
-    print( '\nTotal URLs Scraped : %s ' % str( len( result ) ) )
-    print( 'Script Execution Time : %s ' % ( timer() - start, ) )
- 
- 
+def dorks(search, pages, processes):
+    result = []
+    make_request = partial( get_urls, search )
+    pagelist     = [ str(x*10) for x in range( 0, int(pages) ) ]
+    with Pool(processes) as p:
+        tmp = p.map(make_request, pagelist)
+    for x in tmp:
+        result.extend(x)
+    result=list(set(result))
+    return result
+
